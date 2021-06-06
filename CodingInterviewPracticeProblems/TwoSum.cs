44c50068f29283ef03f8098542cb859647e619a1
Rollerss <oneouthere@gmail.com>
2021-06-06 11:25:41 -0700

    Added TwoSum

---
+++ CodingInterviewPracticeProblems/TwoSum.cs
@@ -0,0 +1,21 @@
+﻿namespace CodingInterviewPracticeProblems
+{
+    public class TwoSum
+    {
+        public int[] GetIndexesTwoNumsEqualTwoSum(int[] numsArr, int sum)
+        {
+            for (int i = 0; i < numsArr.Length; i++)
+            {
+                for (int j = i + 1; j < numsArr.Length; j++)
+                {
+                    if (numsArr[i] + numsArr[j] == sum)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return new int[2];
+        }
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/TwoSum_Test.cs
@@ -0,0 +1,21 @@
+﻿using CodingInterviewPracticeProblems;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class TwoSum_Test
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, 3, new int[] { 0, 1 })]
+        [InlineData(new int[] { 1, 2, 3 }, 4, new int[] { 0, 2 })]
+        [InlineData(new int[] { 1, 2, 3 }, 6, new int[] { 0, 0 })]
+        public void ShouldReturnArrayWithIndexOfNumberEqualToSum(int[] numsArr, int sum, int[] sumIndexes)
+        {
+            var sut = new TwoSum();
+
+            int[] result = sut.GetIndexesTwoNumsEqualTwoSum(numsArr, sum);
+
+            Assert.Equal(sumIndexes, result);
+        }
+    }
+}
