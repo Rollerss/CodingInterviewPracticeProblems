b5e1883d4aacdafa32bd6ee457f4a29f63ff9416
Rollerss <oneouthere@gmail.com>
2021-06-06 15:15:51 -0700

    Added String Reversal

---
+++ CodingInterviewPracticeProblems/StringReversal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace CodingInterviewPracticeProblems
+{
+    public class StringReversal
+    {
+        public string ReverseString(string str)
+        {
+            int strLength = str.Length;
+
+            if (strLength < 2)
+            {
+                return str;
+            }
+
+            char[] reversedChars = new char[strLength];
+
+            for (int i = 0; i < strLength; i++)
+            {
+                reversedChars[i] = str[strLength - 1 - i];
+            }
+
+            return string.Join("", reversedChars);
+        }
+
+        public string FasterReverseString(string str)
+        {
+            int strLength = str.Length;
+
+            if (strLength < 2)
+            {
+                return str;
+            }
+
+            char[] reversedChars = new char[strLength];
+
+            int reverseIndex = strLength - 1;
+
+            for (int i = 0; i < (strLength + 1) / 2; i++)
+            {
+                reverseIndex = strLength - 1 - i;
+
+                reversedChars[i] = str[reverseIndex];
+                reversedChars[reverseIndex] = str[i];
+            }
+
+            return string.Join("", reversedChars);
+        }
+
+        public string ReverseStringBuiltIns(string str) =>
+            string.Join("", str.Reverse());
+
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/StringReversal_Test.cs
@@ -0,0 +1,62 @@
+﻿using CodingInterviewPracticeProblems;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class StringReversal_Test
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("ab", "ba")]
+        [InlineData("abba", "abba")]
+        [InlineData("abab", "baba")]
+        [InlineData("abbbgbbaa", "aabbgbbba")]
+        [InlineData("abbbggbbaa", "aabbggbbba")]
+        [InlineData("abcabc", "cbacba")]
+        public void ShouldReturnReverseString(string str, string answer)
+        {
+            var sut = new StringReversal();
+
+            string result = sut.ReverseString(str);
+
+            Assert.Equal(answer, result);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("ab", "ba")]
+        [InlineData("abba", "abba")]
+        [InlineData("abab", "baba")]
+        [InlineData("abbbgbbaa", "aabbgbbba")]
+        [InlineData("abbbggbbaa", "aabbggbbba")]
+        [InlineData("abcabc", "cbacba")]
+        public void ShouldReturnReverseStringFaster(string str, string answer)
+        {
+            var sut = new StringReversal();
+
+            string result = sut.FasterReverseString(str);
+
+            Assert.Equal(answer, result);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("ab", "ba")]
+        [InlineData("abba", "abba")]
+        [InlineData("abab", "baba")]
+        [InlineData("abbbgbbaa", "aabbgbbba")]
+        [InlineData("abbbggbbaa", "aabbggbbba")]
+        [InlineData("abcabc", "cbacba")]
+        public void ShouldReturnReverseStringUsingBuiltIns(string str, string answer)
+        {
+            var sut = new StringReversal();
+
+            string result = sut.FasterReverseString(str);
+
+            Assert.Equal(answer, result);
+        }
+    }
+}
