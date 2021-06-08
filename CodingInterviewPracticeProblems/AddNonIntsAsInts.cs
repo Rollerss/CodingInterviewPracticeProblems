a6b33173112a333b0a8f029ea003c3548cd4a801
Rollerss <oneouthere@gmail.com>
2021-06-08 01:01:41 -0700

    Added add strings as Ints

---
+++ CodingInterviewPracticeProblems/AddNonIntsAsInts.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CodingInterviewPracticeProblems
+{
+    public class AddNonIntsAsInts
+    {
+        public int SumTwoString(string num1, string num2)
+        {
+            return StringToInt(num1) + StringToInt(num2);
+        }
+
+        private int StringToInt(string num)
+        {
+            int len = num.Length;
+            int value = 0;
+
+            //if (len == 0)
+            //{
+            //    return 0;
+            //}
+
+            for (int i = 0; i < len; i++)
+            {
+                int place = (int)Math.Pow(10, i);
+                value += CharToInt(num[i]) * place;
+            }
+
+            return value;
+        }
+
+        private int CharToInt(char c)
+        {
+            return c switch
+            {
+                '0' => 0,
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 7,
+                '8' => 8,
+                '9' => 9,
+                _ => 0,
+            };
+        }
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/AddNonIntsAsInts_Test.cs
@@ -0,0 +1,23 @@
+﻿using CodingInterviewPracticeProblems;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class AddNonIntsAsInts_Test
+    {
+        [Theory]
+        [InlineData("", "2", 2)]
+        [InlineData("1", "1", 2)]
+        [InlineData("11", "11", 22)]
+        [InlineData("111", "111", 222)]
+
+        public void ShouldReturnArrayWithIndexOfNumberEqualToSum(string num1, string num2, int answer)
+        {
+            var sut = new AddNonIntsAsInts();
+
+            int result = sut.SumTwoString(num1, num2);
+
+            Assert.Equal(answer, result);
+        }
+    }
+}
