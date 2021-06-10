fe990096e22e9201ba34fd932f8eef897eef2185
Rollerss <oneouthere@gmail.com>
2021-06-10 01:23:02 -0700

    positive test of divide two ints without using  built-ins

---
+++ CodingInterviewPracticeProblems/DivideTwoInts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewPracticeProblems
+{
+    public class DivideTwoInts
+    {
+        public int DivideTheLongWay(int divided, int divisor)
+        {
+            int count = 0;
+            while (divided > 0)
+            {
+                divided -= divisor;
+                if (divided >= 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/DivideTwoInts_Test.cs
@@ -0,0 +1,23 @@
+﻿using CodingInterviewPracticeProblems;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class DivideTwoInts_Test
+    {
+        [Theory]
+        [InlineData(1, 2, 0)]
+        [InlineData(1, 1, 1)]
+        [InlineData(11, 2, 5)]
+        [InlineData(111, 10, 11)]
+
+        public void ShouldReturnIntDivided(int num1, int num2, int answer)
+        {
+            var sut = new DivideTwoInts();
+
+            int result = sut.DivideTheLongWay(num1, num2);
+
+            Assert.Equal(answer, result);
+        }
+    }
+}
