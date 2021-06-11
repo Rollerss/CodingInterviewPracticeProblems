544042571973fa33e64bc98f221503accc0d8b00
Rollerss <oneouthere@gmail.com>
2021-06-10 23:15:15 -0700

    added reveres word order in a string

---
+++ CodingInterviewPracticeProblems/ReveresWordOrderInString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace CodingInterviewPracticeProblems
+{
+    public class ReveresWordOrderInString
+    {
+        public string ReveresWordOrderBuiltIns(string str)
+        {
+            if (str.Length < 2 || !str.Contains(" "))
+            {
+                return str;
+            }
+
+            return string.Join(" ", str.Split(" ").Reverse());
+        }
+
+        public string ReveresWordOrder(string str)
+        {
+            if (str.Length < 2 || !str.Contains(" "))
+            {
+                return str;
+            }
+
+            var arr = str.Split(" ");
+
+            string newStr = "";
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                newStr += arr[i];
+
+                if (i != 0)
+                {
+                    newStr += " ";
+                }
+            }
+
+            return newStr.Trim();
+        }
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/ReveresWordOrderInString_Test.cs
@@ -0,0 +1,44 @@
+﻿using CodingInterviewPracticeProblems;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class ReveresWordOrderInString_Test
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("a b", "b a")]
+        [InlineData("ab ba", "ba ab")]
+        [InlineData("ba ba", "ba ba")]
+        [InlineData("hello world", "world hello")]
+        [InlineData("the apple is red", "red is apple the")]
+        [InlineData("the end is near maybe", "maybe near is end the")]
+        public void ShouldReturnReversedWordsInStringBuiltIns(string str, string answer)
+        {
+            var sut = new ReveresWordOrderInString();
+
+            string result = sut.ReveresWordOrderBuiltIns(str);
+
+            Assert.Equal(answer, result);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("a b", "b a")]
+        [InlineData("ab ba", "ba ab")]
+        [InlineData("ba ba", "ba ba")]
+        [InlineData("hello world", "world hello")]
+        [InlineData("the apple is red", "red is apple the")]
+        [InlineData("the end is near maybe", "maybe near is end the")]
+        public void ShouldReturnReversedWordsInString(string str, string answer)
+        {
+            var sut = new ReveresWordOrderInString();
+
+            string result = sut.ReveresWordOrder(str);
+
+            Assert.Equal(answer, result);
+        }
+    }
+}
