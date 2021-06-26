7abba780586b52f730851bcff116a7928d2da4db
Rollerss <oneouthere@gmail.com>
2021-06-25 23:12:21 -0700

    Added Find Duplicate Number In Array

---
+++ CodingInterviewPracticeProblems/FindDuplicateNumber.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingInterviewPracticeProblems
+{
+    public class FindDuplicateNumber
+    {
+        public int FindDuplicateNumberInArray(int[] nums)
+        {
+            HashSet<int> numSet = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (numSet.Contains(num))
+                {
+                    return num;
+                }
+                numSet.Add(num);
+            }
+
+            return 0;
+        }
+
+        public int FindDuplicateNumberInArrayForeachLinq(int[] nums)
+        {
+            foreach (var num in nums)
+            {
+                if (nums.Count(n => n == num) > 1)
+                {
+                    return num;
+                }
+            }
+
+            return 0;
+        }
+
+        public int FindDuplicateNumberInArrayLinq2(int[] nums)
+        {
+            return nums.GroupBy(x => x)
+                       .Where(n => n.Count() > 1)
+                       .Select(g => g.Key)
+                       .FirstOrDefault();
+        }
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/FindDuplicateNumber_Test.cs
@@ -0,0 +1,53 @@
+﻿using CodingInterviewPracticeProblems;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class FindDuplicateNumber_Test
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 0)]
+        [InlineData(new int[] { 2, 4, 6, 2 }, 2)]
+        [InlineData(new int[] { 3, 1, 2, 3, 4, 5, 6 }, 3)]
+
+        public void ShouldRetureDupNumHashSet(int[] nums, int answer)
+        {
+            var sut = new FindDuplicateNumber();
+
+            int result = sut.FindDuplicateNumberInArray(nums);
+
+            Assert.Equal(answer, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 0)]
+        [InlineData(new int[] { 2, 4, 6, 2 }, 2)]
+        [InlineData(new int[] { 3, 1, 2, 3, 4, 5, 6 }, 3)]
+
+        public void ShouldRetureDupNumLinq(int[] nums, int answer)
+        {
+            var sut = new FindDuplicateNumber();
+
+            int result = sut.FindDuplicateNumberInArrayForeachLinq(nums);
+
+            Assert.Equal(answer, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 0)]
+        [InlineData(new int[] { 2, 4, 6, 2 }, 2)]
+        [InlineData(new int[] { 3, 1, 2, 3, 4, 5, 6 }, 3)]
+
+        public void ShouldRetureDupNumLinq2(int[] nums, int answer)
+        {
+            var sut = new FindDuplicateNumber();
+
+            int result = sut.FindDuplicateNumberInArrayLinq2(nums);
+
+            Assert.Equal(answer, result);
+        }
+    }
+}
