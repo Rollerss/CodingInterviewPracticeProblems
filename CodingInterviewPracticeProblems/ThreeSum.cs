d080e53c7af157b293821e38296faa2e0ddeb939
Rollerss <oneouthere@gmail.com>
2021-07-07 00:43:26 -0700

    Added three sum

---
+++ CodingInterviewPracticeProblems/ThreeSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingInterviewPracticeProblems
+{
+    public class ThreeSum
+    {
+        public int[][] GetTripletsEqualToZero(int[] arrNums)
+        {
+            HashSet<int[]> arrAnsw = new HashSet<int[]>();
+
+            int arrLen = arrNums.Length;
+
+            for (int i = 0; i < arrLen; i++)
+            {
+                for (int j = i + 1; j < arrLen; j++)
+                {
+                    for (int k = j + 1; k < arrLen; k++)
+                    {
+                        if (arrNums[i] + arrNums[j] + arrNums[k] == 0)
+                        {
+                            int[] answ = { arrNums[i], arrNums[j], arrNums[k] };
+                            Array.Sort(answ);
+                            if (!arrAnsw.Any(a => a.SequenceEqual(answ)))
+                            {
+                                arrAnsw.Add(answ);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return arrAnsw.ToArray();
+        }
+
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/ThreeSum_Test.cs
@@ -0,0 +1,38 @@
+﻿using CodingInterviewPracticeProblems;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class ThreeSum_Test
+    {
+        // https://leetcode.com/problems/3sum/
+        // Given an integer array nums, return all the triplets[nums[i], nums[j], nums[k]]
+        // such that i != j, i != k, and j != k,
+        // and nums[i] + nums[j] + nums[k] == 0.
+        // Notice that the solution set must not contain duplicate triplets.
+
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, -3 }, new int[] { -3, 1, 2 })]
+        [InlineData(new int[] { 1, 2, 3, -4 }, new int[] { -4, 1, 3 })]
+        [InlineData(new int[] { 1, 2, 3, -5 }, new int[] { -5, 2, 3, })]
+        [InlineData(new int[] { 1, 2, 3, -5, 2 }, new int[] { -5, 2, 3 })]
+        [InlineData(new int[] { 1, 2, 3, -5, 2, 4 }, new int[] { -5, 1, 4 }, new int[] { -5, 2, 3 })]
+        [InlineData(new int[] { 1, 2, 3, -5, 2, 4 }, new int[] { -5, 2, 3 }, new int[] { -5, 1, 4 })]
+        public void ShouldReturnArrayOfArraysEqualToZero(int[] numsArr, params int[][] arrAnswer)
+        {
+            var sut = new ThreeSum();
+
+            int[][] result = sut.GetTripletsEqualToZero(numsArr);
+
+            foreach (var answer in arrAnswer)
+            {
+                Assert.Contains(result, a => a.SequenceEqual(answer));
+            }
+
+            Assert.Equal(arrAnswer.Length, result.Length);
+        }
+    }
+}
