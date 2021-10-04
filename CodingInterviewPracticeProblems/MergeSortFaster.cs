36d5c488ac264bbbef4d03c8e13d7fe3e4e2b7dd
Rollerss <oneouthere@gmail.com>
2021-10-04 00:46:23 -0700

    add a Merge Sort with a O(n) best case

---
+++ CodingInterviewPracticeProblems/MergeSortFaster.cs
@@ -0,0 +1,81 @@
+﻿namespace CodingInterviewPracticeProblems
+{
+    public class MergeSortFaster
+    {
+        public int[] FasterMergeSort(int[] itemsArr)
+        {
+            if (CheckIfSorted(itemsArr) == itemsArr.Length)
+            {
+                return itemsArr;
+            }
+            return MergeSort(itemsArr);
+        }
+
+        public int[] FasterMergeSort2(int[] arr)
+        {
+            int middle = CheckIfSorted(arr);
+            if (middle == arr.Length)
+            {
+                return arr;
+            }
+            int[] arrLeft = arr[0..middle];
+            int[] arrRight = MergeSort(arr[middle..]);
+            return MergeArr(arrLeft, arrRight);
+        }
+
+        private int CheckIfSorted(int[] itemsArr)
+        {
+            for (int i = 0; i < itemsArr.Length - 1; i++)
+            {
+                if (itemsArr[i] > itemsArr[i + 1])
+                {
+                    return i;
+                }
+            }
+            return itemsArr.Length;
+        }
+
+        private int[] MergeSort(int[] arr)
+        {
+            if (arr.Length == 1)
+            {
+                return arr;
+            }
+            int middle = arr.Length / 2;
+            int[] arrLeft = MergeSort(arr[0..middle]);
+            int[] arrRight = MergeSort(arr[middle..]);
+            return MergeArr(arrLeft, arrRight);
+        }
+
+        private int[] MergeArr(int[] arr1, int[] arr2)
+        {
+            int len1 = arr1.Length;
+            int len2 = arr2.Length;
+            int[] newArr = new int[len1 + len2];
+            int index1 = 0;
+            int index2 = 0;
+            int indexNew = 0;
+
+            while (index1 < len1 || index2 < len2)
+            {
+                if (index2 == len2)
+                {
+                    newArr[indexNew++] = arr1[index1++];
+                }
+                else if (index1 == len1)
+                {
+                    newArr[indexNew++] = arr2[index2++];
+                }
+                else if (arr1[index1] < arr2[index2])
+                {
+                    newArr[indexNew++] = arr1[index1++];
+                }
+                else
+                {
+                    newArr[indexNew++] = arr2[index2++];
+                }
+            }
+            return newArr;
+        }
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/MergeSortFaster_Test.cs
@@ -0,0 +1,53 @@
+﻿using CodingInterviewPracticeProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class MergeSortFaster_Test
+    {
+        private readonly ITestOutputHelper output;
+
+        public MergeSortFaster_Test(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 4, 6, 1, 3, 5 }, new int[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(new int[] { 2, 4, 6, 7, 1, 3, 5 }, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        [InlineData(new int[] { 2, 4, 6, 6, 1, 3, 5 }, new int[] { 1, 2, 3, 4, 5, 6, 6 })]
+        [InlineData(new int[] { 1, 3, 5 }, new int[] { 1, 3, 5 })]
+        [InlineData(new int[] { 2, 4, 6 }, new int[] { 2, 4, 6 })]
+        public void FasterMergeSort(int[] arr1, int[] answer)
+        {
+            var sut = new MergeSortFaster();
+
+            var combindedArr = sut.FasterMergeSort(arr1);
+
+            Assert.Equal(answer, combindedArr);
+
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 4, 6, 1, 3, 5 }, new int[] { 1, 2, 3, 4, 5, 6 })]
+        [InlineData(new int[] { 2, 4, 6, 7, 1, 3, 5 }, new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        [InlineData(new int[] { 2, 4, 6, 6, 1, 3, 5 }, new int[] { 1, 2, 3, 4, 5, 6, 6 })]
+        [InlineData(new int[] { 1, 3, 5 }, new int[] { 1, 3, 5 })]
+        [InlineData(new int[] { 2, 4, 6 }, new int[] { 2, 4, 6 })]
+        public void FasterMergeSort2(int[] arr1, int[] answer)
+        {
+            var sut = new MergeSortFaster();
+
+            var combindedArr = sut.FasterMergeSort2(arr1);
+
+            Assert.Equal(answer, combindedArr);
+
+        }
+    }
+}
