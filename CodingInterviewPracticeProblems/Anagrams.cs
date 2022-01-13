a1a74cb5da06ab1d067459a937dffebe00a4fbd6
Rollerss <oneouthere@gmail.com>
2022-01-12 23:45:05 -0800

    Added Anagram checker

---
+++ CodingInterviewPracticeProblems/Anagrams.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingInterviewPracticeProblems
+{
+    public class Anagrams
+    {
+        public bool AreTwoStringsAnagrams(string strOne, string strTwo)
+        {
+            if(strOne.Count() != strTwo.Count()) return false;
+            var hashOne = CreateHashtable(strOne);
+            var hashTwo = CreateHashtable(strTwo);
+            return CompareHashtable(hashOne, hashTwo);
+        }
+
+        private bool CompareHashtable(Hashtable hashOne, Hashtable hashTwo)
+        {
+            if (hashOne.Count != hashTwo.Count) return false;
+
+            foreach (DictionaryEntry h in hashOne)
+            {
+                if ((int)h.Value != (int)hashTwo[h.Key]) return false;
+            }
+            return true;
+        }
+
+        private Hashtable CreateHashtable(string str)
+        {
+            var hash = new Hashtable();
+
+            foreach (char c in str)
+            {
+                if (hash.ContainsKey(c))
+                {
+                    hash[c] = (int)hash[c] + 1;
+                }
+                else
+                {
+                    hash[c] = 1;
+                }
+            }
+
+            return hash;
+        }
+
+        public bool AreStringsAnagrams(string strOne, string strTwo)
+        {
+            if (strOne.Count() != strTwo.Count()) return false;
+            var dictOne = CreateDictionary(strOne);
+            var dictTwo = CreateDictionary(strTwo);
+            return CompareDictionary(dictOne, dictTwo);
+        }
+
+        private bool CompareDictionary(Dictionary<char, int> dictOne, Dictionary<char, int> dictTwo)
+        {
+            if(dictOne.Count != dictTwo.Count) return false;
+
+            foreach (var (k,v) in dictOne)
+            {
+                if(v != dictTwo[k]) return false;
+            }
+            return true;
+        }
+
+        private Dictionary<char, int> CreateDictionary(string str)
+        {
+            var dict = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c] += 1;
+                }
+                else
+                {
+                    dict[c] = 1;
+                }
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ Test_CodingInterviewPracticeProblems/Anagrams_Test.cs
@@ -0,0 +1,32 @@
+﻿using CodingInterviewPracticeProblems;
+using Xunit;
+
+namespace Test_CodingInterviewPracticeProblems
+{
+    public class Anagrams_Test
+    {
+        [Theory]
+        [InlineData("abc", "cba", true)]
+        [InlineData("danger", "garden", true)]
+        [InlineData("aab", "bba", false)]
+        [InlineData("aab", "ba", false)]
+        public void ShouldReturnIfTwoStringsAreAnagramsDict(string strOne, string strTwo, bool expected)
+        {
+            var sut = new Anagrams();
+            bool result = sut.AreStringsAnagrams(strOne, strTwo);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("abc", "cba", true)]
+        [InlineData("danger", "garden", true)]
+        [InlineData("aab", "bba", false)]
+        [InlineData("aab", "ba", false)]
+        public void ShouldReturnIfTwoStringsAreAnagramsHash(string strOne, string strTwo, bool expected)
+        {
+            var sut = new Anagrams();
+            bool result = sut.AreTwoStringsAnagrams(strOne, strTwo);
+            Assert.Equal(expected, result);
+        }
+    }
+}
